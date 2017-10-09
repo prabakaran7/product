51804007d2af84828f67d254083ec80b4731d4c1
user <user@hp-PC>
2017-10-09 11:43:20 +0530

    controller change

--- mypro/src/com/mypro/controller/HelloWorldController.java
+++ mypro/src/com/mypro/controller/HelloWorldController.java
@@ -1,13 +1,16 @@
 package com.mypro.controller;
 
-import org.springframework.stereotype.Controller;  
-import org.springframework.web.bind.annotation.RequestMapping;  
-import org.springframework.web.servlet.ModelAndView;  
-@Controller  
-public class HelloWorldController {  
-    @RequestMapping("/hello")  
-    public ModelAndView helloWorld() {  
-        String message = "HELLO SPRING MVC HOW R U";  
-        return new ModelAndView("hellopage", "message", message);  
-    }  
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public ModelAndView helloWorld() {
+		String message = "HELLO SPRING MVC HOW R U";
+		return new ModelAndView("hellopage", "message", message);
+	}
 }
