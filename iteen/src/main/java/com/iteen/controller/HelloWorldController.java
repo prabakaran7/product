210a38d6d813835f12aba0f85e21c55b16b2304e
user <user@hp-PC>
2017-10-15 13:39:03 +0530

    adding ping servlet

--- iteen/src/main/java/com/iteen/controller/HelloWorldController.java
+++ iteen/src/main/java/com/iteen/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.iteen.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,5 +14,10 @@ public class HelloWorldController {
 	public ModelAndView helloWorld() {
 		String message = "HELLO SPRING MVC HOW R U";
 		return new ModelAndView("hellopage", "message", message);
+	}
+	
+	@RequestMapping(value = "/pingServlet", method = RequestMethod.GET)
+	public ResponseEntity<String> pingServlet(){
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
